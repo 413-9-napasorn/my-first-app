ef35334a138b2ac039520a3c5216b0702703973f
413-9-napasorn <54267@yupparaj.ac.th>
2026-08-24 22:39:21 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -30,14 +30,14 @@ def show_result_dialog(ans1, ans2):
     u_ans2 = ans2.strip().lower()
 
     # ตรวจข้อ 1
-    if u_ans1 == "apple":
+    if u_ans1 == "orange":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     # ตรวจข้อ 2
-    if u_ans2 == "fish":
+    if u_ans2 == "car":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
@@ -72,11 +72,11 @@ st.divider()
 
 # 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
 ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    "ข้อ 1: An `o _ a n g e ` is full of vitamin c . 🍊",
     value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    "ข้อ 2: he drives a red `c _ r`to write . 🚗",
     value=st.session_state.ans2_val,
 )
 
